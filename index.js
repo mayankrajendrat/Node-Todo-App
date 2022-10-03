d4bf89252174169d1e1dd58e0b0b64b87d65c233
Mayank Tiwari <mayank_tiwari@epam.com>
2022-10-04 01:41:09 +0530

    Modified MongoDB URI

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ const port = 8000;
 const todoRoutes = require("./routes/Todo");
 
 const app = express();
-const uri = process.env.MONGODB_URI;
-//const uri = "mongodb+srv://mayankrajendrat:1122334455@cluster0.gjy2keg.mongodb.net/?retryWrites=true&w=majority";
+//const uri = process.env.MONGODB_URI;
+const uri = "mongodb+srv://mayankrajendrat:1122334455@cluster0.gjy2keg.mongodb.net/?retryWrites=true&w=majority";
 
 // DB connection
 mongoose
